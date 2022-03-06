ec3dd93b5bcb1e8e00a469f9f4752ade1916539b
bquan0 <powerbowen@gmail.com>
2022-03-06 12:31:18 -0600

    implemented error state

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,22 @@
 #include "ina.h"
 // no clue what it's supposed to be
-#define BUTTON A0
+#define BUTTON D8
+#define BATT_CHECK A0
+#define MCU_SET D3
+#define MCU_RELAY_EN D2
+#define MCU_CHRG_STAT A7
+#define MCU_CHRG_EN D13
 
-typedef enum {idle, ready, error, testing, finish} state_t;
+typedef enum {idle, ready, error, testCharge, testDischarge, finish} state_t;
 state_t state;
 
 void setup() {
   pinMode(BUTTON, INPUT);
+  pinMode(BATT_CHECK, INPUT);
+  pinMode(MCU_SET, OUTPUT);
+  pinMode(MCU_RELAY_EN, OUTPUT);
+  pinMode(MCU_CHRG_STAT, INPUT);
+  pinMode(MCU_CHRG_EN, OUTPUT);
 
   Serial.begin(115200);
   while(!Serial) {}
@@ -27,8 +37,11 @@ void loop() {
     case error:
       state = errorState();
       break;
-    case testing:
-      state = testingState();
+    case testCharge:
+      state = testChargeState();
+      break;
+    case testDischarge:
+      state = testDischargeState();
       break;
     case finish:
       state = finishState();
@@ -42,13 +55,13 @@ bool buttonPressed(void) {
 }
 
 unsigned char battCheck(void) {
-  // inserted correctly
-  return 0;
-  // inserted backward
-  return 1;
-  // not inserted
-  return 2;
-
+  int voltage = analogRead(BATT_CHECK);
+  if(voltage == 0){
+    return 1; // battery not inserted
+  }
+  else{
+    return 0; // battery inserted (may be inserted correctly OR may be backwards)
+  }
 }
 
 state_t idleState(void) {
@@ -69,20 +82,50 @@ state_t readyState() {
   return ready;
 }
 state_t errorState() {
-  // Stop charge/Discharge
-  // Isolate Battery (switch relay)
+  // Stop charge/Discharge. MCU_SET
+  // Isolate Battery (switch relay). MCU_RELAY_EN
+  analogWrite(MCU_SET, 0); 
+  digitalWrite(MCU_RELAY_EN, 0);
+  
   // Display error message
- 
+  Serial.println("error");
+  
   return error;
 }
-state_t testingState() {
-  // Charge/discharge
-  // Monitor/display parameters
-  // Display "Test in Progress" and estimated time left
+state_t testChargeState(){
+  // Check battery
+  if(battCheck() != 0){
+    return error;
+  }
+  // Enable charging
+  digitalWrite(MCU_RELAY_EN, 0);
+  digitalWrite(MCU_CHRG_EN, 1);
+  // stop charging when MCU_CHRG_STAT is 0
+  while(analogRead(MCU_CHRG_STAT) != 0){
+    
+  }
+  // Read status of Orange LED
+ 
+  // return error;
+  // return finish; 
+  return testCharge;
+}
+state_t testDischargeState() {
+  // set constant current (use MCU_SET voltage)
+  analogWrite(MCU_SET, 0); // change this value
+  // check battery status (BAT_CHECK)
+  if(battCheck() != 0){
+    return error;
+  }
+  // switch relay to discharge
+  // monitor battery voltage
+  // monitor thermistor temperatures
+  // timer
+  // stop test (switch off relay) when minimum voltage is reached
 
   // return error;
   // return finish;
-  return testing;
+  return testDischarge;
 }
 state_t finishState() {
   // Stop discharging, isolate cell
